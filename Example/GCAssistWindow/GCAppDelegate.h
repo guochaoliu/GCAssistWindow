76dc3de65fb51382522eae3e085564673cd42d28
guochao <guo_chao_liu@sina.com>
2020-08-18 17:12:19 +0800

    Initial commit

---
+++ Example/GCAssistWindow/GCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GCAppDelegate.h
+//  GCAssistWindow
+//
+//  Created by guochaoliu on 08/18/2020.
+//  Copyright (c) 2020 guochaoliu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/GCAssistWindow/GCViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCViewController.h
+//  GCAssistWindow
+//
+//  Created by guochaoliu on 08/18/2020.
+//  Copyright (c) 2020 guochaoliu. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GCViewController : UIViewController
+
+@end
